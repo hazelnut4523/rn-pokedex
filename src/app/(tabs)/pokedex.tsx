9156b66809b2a2132cd788778741cddea3a0ca04
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-08-06 16:25:22 +0900

    도감 페이지 무한스크롤

--- src/app/(tabs)/pokedex.tsx
+++ src/app/(tabs)/pokedex.tsx
@@ -1,13 +1,26 @@
 import { PokemonCard } from "@/src/components/card/PokemonCard";
+import { useState } from "react";
 import { FlatList, Text, View } from "react-native";
 
 export default function PokedexScreen() {
+  const [array, setArray] = useState<number[]>(
+    new Array(10).fill(null).map((_, index) => index + 1),
+  );
+
+  const handleEndReached = () => {
+    setArray((val) => [
+      ...val,
+      ...new Array(10).fill(null).map((_, idx) => val.length + idx + 1),
+    ]);
+  };
+
   return (
     <View className="flex-1 px-4 pt-4 flex flex-col gap-4">
       <Text className="text-[32px] font-bold font-[DungGeunMo]">도감</Text>
 
       <FlatList
-        data={new Array(50).fill(null).map((_, index) => index + 1)}
+        data={array}
+        onEndReached={handleEndReached}
         renderItem={({ item }) => (
           <View className="w-1/2 p-2">
             <PokemonCard pokemonId={item} />
